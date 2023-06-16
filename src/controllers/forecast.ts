fc4ffd6d629da2882a4404462aaa3a4491237408
gominsz <matheuscamargodealencar@gmail.com>
2023-06-16 02:13:47 -0300

    error-forecast

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -1,11 +1,11 @@
 import { Controller, Get } from '@overnightjs/core'
 import { Request, Response } from 'express';
 
-@Controller('forcast')
+@Controller('forecast')
 export class ForecastController {
 
   @Get('')
-  public getForcastForLoggedUser(_: Request, res: Response): void {
+  public getForecastForLoggedUser(_: Request, res: Response): void {
     res.send([{
       "time": "2020-04-26T00:00:00+00:00",
       "forecast": [{
