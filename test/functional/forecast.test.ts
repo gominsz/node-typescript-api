7f19e9859ceaaf5d45d943a621998d97736a7d0c
gominsz <matheuscamargodealencar@gmail.com>
2023-06-16 02:15:23 -0300

    debug-getApp/sintaxe

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -9,26 +9,26 @@ describe('Beach forecast functional tests', () => {
   beforeAll(() => {
     const server = new SetupServer();
     server.init();
-      request = supertest(server.getApp)
+      request = supertest(server.getApp())
   });
- it('should return a forecast with just a few times', async() => {
-  const { body, status } = await request.get('/forecast');
-  expect(status).toBe(200);
-  expect(body).toEqual([{
-    "time": "2020-04-26T00:00:00+00:00",
-    "forecast": [{
-      "lat": -33.792726,
-      "lng": 151.289824,
-      "name": "Manly",
-      "position": "E",
-      "rating": 2,
-      "swellDirection": 64.26,
-      "swellHeight": 0.15,
-      "swellPeriod": 3.89,
+  it('should return a forecast with just a few times', async() => {
+    const { body, status } = await request.get('/forecast');
+    expect(status).toBe(200);
+    expect(body).toEqual([{
       "time": "2020-04-26T00:00:00+00:00",
-      "waveDirection": 231.38,
-      "waveHeight": 0.47,
-      "windDirection": 299.45
+      "forecast": [{
+       "lat": -33.792726,
+        "lng": 151.289824,
+        "name": "Manly",
+        "position": "E",
+        "rating": 2,
+        "swellDirection": 64.26,
+        "swellHeight": 0.15,
+        "swellPeriod": 3.89,
+        "time": "2020-04-26T00:00:00+00:00",
+        "waveDirection": 231.38,
+        "waveHeight": 0.47,
+        "windDirection": 299.45
     }]
   }, {
     "time": "2020-04-26T01:00:00+00:00",
