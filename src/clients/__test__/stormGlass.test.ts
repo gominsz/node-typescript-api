a5d08a9797e730eccf5eab63011b2b81d51c6ae7
gominsz <matheuscamargodealencar@gmail.com>
2023-06-21 16:15:35 -0300

    Add test case for fetching normalized forecast from StormGlass service

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,12 +1,19 @@
 import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios';
+import stormGlassWeather3HoursFicture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalizedWeather3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+
+jest.mock('axios');
 
 describe('StormGlass client', () => {
   it('should return the normalized forecast from the StormGlass service', async () => {
     const lat = -33.792726;
     const lng = 151.289824;
 
-    const stormTGlass = new StormGlass();
+    axios.get = jest.fn().mockReturnValue(stormGlassWeather3HoursFicture);
+
+    const stormTGlass = new StormGlass(axios);
     const response = await stormTGlass.fetchPoints(lat, lng);
-    expect(response).toEqual({});
+    expect(response).toEqual(stormGlassNormalizedWeather3HoursFixture);
   });
 });
