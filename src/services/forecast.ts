17acc8205d8b337a86bf76f782f9b5b9d9556b74
gominsz <matheuscamargodealencar@gmail.com>
2023-06-30 16:21:12 -0300

    Adicionar classe Forecast para processar previsões de praia com StormGlass e mapear por tempo

---
+++ src/services/forecast.ts
@@ -0,0 +1,65 @@
+import { StormGlass, ForecastPoint } from '@src/clients/stormGlass';
+
+export enum BeachPosition {
+  S = 'S',
+  E = 'E',
+  W = 'W',
+  N = 'N',
+}
+
+export interface Beach {
+  name: string;
+  position: BeachPosition;
+  lat: number;
+  lng: number;
+  user: string;
+}
+
+export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
+
+export interface TimeForecast {
+  time: string;
+  forecast: BeachForecast[];
+}
+
+export class Forecast {
+  constructor(protected stormGlass = new StormGlass()) {}
+
+  public async processForecastForBeaches(
+    beaches: Beach[]
+  ): Promise<TimeForecast[]> {
+    const pointsWithCorrectSources: BeachForecast[] = [];
+    for (const beach of beaches) {
+      const points = await this.stormGlass.fetchPoints(beach.lat, beach.lng);
+      const enrichedBeachData = points.map((e) => ({
+        ...{},
+        ...{
+          lat: beach.lat,
+          lng: beach.lng,
+          name: beach.name,
+          position: beach.position,
+          rating: 1, //need to be implemented
+        },
+        ...e,
+      }));
+      pointsWithCorrectSources.push(...enrichedBeachData);
+    }
+    return this.mapForecastByTime(pointsWithCorrectSources);
+  }
+
+  private mapForecastByTime(forecast: BeachForecast[]): TimeForecast[] {
+    const forecastByTime: TimeForecast[] = [];
+    for (const point of forecast) {
+      const timePoint = forecastByTime.find((f) => f.time === point.time);
+      if (timePoint) {
+        timePoint.forecast.push(point);
+      } else {
+        forecastByTime.push({
+          time: point.time,
+          forecast: [point],
+        });
+      }
+    }
+    return forecastByTime;
+  }
+}
