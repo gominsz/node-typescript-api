0377cc58dc60fb5b1ee2f11bae6259c27b67be09
gominsz <matheuscamargodealencar@gmail.com>
2023-07-14 14:23:50 -0300

    declare global

--- test/global.d.ts
+++ test/global.d.ts
@@ -1,6 +1,10 @@
-//adicionando a funcionalidade testRequest no global
-declare namespace NodeJS {
-  interface Global {
-    testRequest: import('supertest').SuperTest<import('supertest').Test>;
-  }
+/**
+ * @Update 2022 - This has logic has been update to support newer Node.js versions
+ * that don't have NodeJS as a global type
+ */
+declare global {
+  //eslint-disable-next-line no-var
+  var testRequest: import('supertest').SuperTest<import('supertest').Test>;
 }
+
+export {};
