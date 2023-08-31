f8221dbfdef1d8838d7f76129b789b2d602d4d0f
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-08-31 18:59:56 -0300

    starting the applications (server.start) - 3333

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,7 @@
+import { SetupServer } from './server';
+
 console.log('Ta funcionando RELAXA!!');
+const server = new SetupServer(3333);
+server.init().then(() => {
+  server.start(() => console.log('abriu'));
+});
