c7da6f48d9ece2c4bffd4549720ae903bb28c86e
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-08-31 18:52:23 -0300

    debug testMatch (src) for(test)

--- jest.config.js
+++ jest.config.js
@@ -3,7 +3,7 @@ const root = resolve(__dirname);
 module.exports = {
   rootDir: root,
   displayName: 'root-tests',
-  testMatch: ['<rootDir>/src/**/*.test.ts'],
+  testMatch: ['<rootDir>/test/**/*.test.ts'],
   testEnvironment: 'node',
   clearMocks: true,
   preset: 'ts-jest',
