5712ee2f3f6733ef8b989fbbb53eec207fb646ad
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-08-31 19:05:59 -0300

    SetupServer (3000)

--- test/jest-setup.ts
+++ test/jest-setup.ts
@@ -2,9 +2,12 @@ import { SetupServer } from '@src/server';
 import supertest from 'supertest';
 
 let server: SetupServer;
+
 beforeAll(async () => {
-  server = new SetupServer();
+  server = new SetupServer(3000);
+
   await server.init();
+
   global.testRequest = supertest(server.getApp());
 });
 
