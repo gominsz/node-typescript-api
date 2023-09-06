5ca40bc9e60e1383fae8d08b93356601d0c9a90a
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-09-06 17:08:50 -0300

    Error for Beaches end Error validator instanceof

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,10 +1,23 @@
 import { Controller, Post } from '@overnightjs/core';
+import { Beach } from '@src/models/beache';
 import { Request, Response } from 'express';
+import mongoose from 'mongoose';
 
 @Controller('beaches')
 export class BeachesController {
   @Post('')
   public async create(req: Request, res: Response): Promise<void> {
-    res.status(201).send({ ...req.body, id: 'fake-id' });
+    try {
+      const beach = new Beach(req.body);
+      const result = await beach.save();
+      res.status(201).send(result);
+    } catch (error: unknown) {
+      // 'unknown' para evitar o erro de tipo desconhecido
+      if (error instanceof mongoose.Error.ValidationError) {
+        res.status(422).send({ error: (error as Error).message });
+      } else {
+        res.status(500).send({ error: 'Internal Server Error' });
+      }
+    }
   }
 }
