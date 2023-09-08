2bb274c98ebe996a7a6085ec186a8ccf688126a3
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-09-08 18:26:42 -0300

    deleted user (Beach)

--- src/services/forecast.ts
+++ src/services/forecast.ts
@@ -16,8 +16,7 @@ export interface Beach {
   name: string;
   position: BeachPosition;
   lat: number;
-  lng: number;
-  user: string;
+  lng: number; 
 }
 
 export interface BeachForecast extends Omit<Beach, 'user'>, ForecastPoint {}
