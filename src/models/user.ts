59543eaef54b0e658119d837c3badea56009f5ef
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-09-08 18:38:48 -0300

    add users (src/models) - interface User - Model<UserDocument

---
+++ src/models/user.ts
@@ -0,0 +1,36 @@
+import mongoose, { Document, Model } from 'mongoose';
+
+export interface User {
+  _id?: string;
+  name: string;
+  email: string;
+  password: string;
+}
+
+type UserDocument = User & Document;
+
+const schema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: { type: String, required: true },
+  },
+  {
+    toJSON: {
+      transform: (_, ret): void => {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+export const User: Model<UserDocument> = mongoose.model<UserDocument>(
+  'User',
+  schema
+);
