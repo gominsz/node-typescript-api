bbb91826b7975a916790144ffdc8c16e67730492
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-09-08 18:32:06 -0300

    src/index add start server (config/SetupServer/App.port)

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,17 @@
+import { promises } from 'dns';
 import { SetupServer } from './server';
+import config from 'config';
 
+(async (): Promise<void> => {
+  const server = new SetupServer(config.get('App.port'));
+  await server.init();
+  server.start();
+})();
+
+/*
 console.log('Ta funcionando RELAXA!!');
 const server = new SetupServer(3333);
 server.init().then(() => {
   server.start(() => console.log('abriu'));
 });
+*/
