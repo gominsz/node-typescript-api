43c2da177fbb2b3348cdf1469caa0862eb0f24b7
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-09-25 14:43:11 -0300

    CUSTOM_VALIDATION.DUPLICATED User valid

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,10 +1,15 @@
 import mongoose, { Document, Model } from 'mongoose';
+import AuthService from '@src/services/auth';
 
 export interface User {
   _id?: string;
   name: string;
   email: string;
   password: string;
+}
+
+export enum CUSTOM_VALIDATION {
+  DUPLICATED = 'DUPLICATED',
 }
 
 type UserDocument = User & Document;
@@ -29,6 +34,31 @@ const schema = new mongoose.Schema(
     },
   }
 );
+
+/**
+ * Validates the email and throws a validation error, otherwise it will throw a 500
+ */
+schema.path('email').validate(
+  async (email: string) => {
+    const emailCount = await mongoose.models.User.countDocuments({ email });
+    return !emailCount;
+  },
+  'already exists in the database.',
+  CUSTOM_VALIDATION.DUPLICATED
+);
+
+schema.pre('save', async function (): Promise<void> {
+  if (!this.password || !this.isModified('password')) {
+    return;
+  }
+
+  try {
+    const hashedPassword = await AuthService.hashPassword(this.password);
+    this.password = hashedPassword;
+  } catch (err) {
+    console.error('Error hashing the password for the user ${this.name}');
+  }
+});
 
 export const User: Model<UserDocument> = mongoose.model<UserDocument>(
   'User',
