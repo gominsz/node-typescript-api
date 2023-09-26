f72cef50d317c1053b6720ab0efcb1abd522b90d
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-09-26 13:57:06 -0300

    Post authenticate AuthService.comparePassword (Error) password does not match or user not found

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -2,6 +2,7 @@ import { Controller, Post } from '@overnightjs/core';
 import { Response, Request } from 'express';
 import { User } from '@src/models/user';
 import { BaseController } from './index';
+import AuthService from '@src/services/auth';
 
 @Controller('users')
 export class UsersController extends BaseController {
@@ -14,5 +15,28 @@ export class UsersController extends BaseController {
     } catch (error) {
       this.sendCreateUpdateErrorResponse(res, error);
     }
+  }
+
+  @Post('authenticate')
+  public async authenticate(
+    req: Request,
+    res: Response
+  ): Promise<Response | undefined> {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(401).send({
+        code: 401,
+        error: 'user not found!',
+      });
+    }
+    if (!(await AuthService.comparePassword(password, user.password))) {
+      return res.status(401).send({
+        code: 401,
+        error: 'password does not match!',
+      });
+    }
+    const token = AuthService.generateToken(user.toJSON());
+    return res.status(200).send({ token: token });
   }
 }
