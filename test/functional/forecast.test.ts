fc21228084c6db08092642761c3fc7288036e6bf
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-10-02 17:16:11 -0300

    implementing a defaultUse test user (John Doe) and deleteMany/x-access-token

--- test/functional/forecast.test.ts
+++ test/functional/forecast.test.ts
@@ -1,19 +1,30 @@
 import { Beach, BeachPosition } from '@src/models/beache';
 import nock from 'nock';
-import apiForecastResponse1BeachFixture from '@test/fixtures/api_forecast_response_1_beach.json';
-import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassWeather3HoursFixture from '../fixtures/stormglass_weather_3_hours.json';
+import apiForecastResponse1BeachFixture from '../fixtures/api_forecast_response_1_beach.json';
+import { User } from '@src/models/user';
+import AuthService from '@src/services/auth';
 
 describe('Beach forecast functional tests', () => {
+  const defaultUser: User = {
+    name: 'John Doe',
+    email: 'john3@mail.com',
+    password: '1234',
+  };
+  let token: string;
   beforeEach(async () => {
     await Beach.deleteMany({});
+    await User.deleteMany({});
+    const user = await new User(defaultUser).save();
     const defaultBeach = {
       lat: -33.792726,
       lng: 151.289824,
       name: 'Manly',
       position: BeachPosition.E,
+      user: user.id,
     };
-    const beach = new Beach(defaultBeach);
-    await beach.save();
+    await new Beach(defaultBeach).save();
+    token = AuthService.generateToken(user.toJSON());
   });
   it('should return a forecast with just a few times', async () => {
     nock('https://api.stormglass.io:443', {
@@ -32,11 +43,14 @@ describe('Beach forecast functional tests', () => {
       })
       .reply(200, stormGlassWeather3HoursFixture);
 
-    const { body, status } = await globalThis.testRequest.get('/forecast');
+    const { body, status } = await global.testRequest
+      .get('/forecast')
+      .set({ 'x-access-token': token });
     expect(status).toBe(200);
     // Make sure we use toEqual to check value not the object and array itself
     expect(body).toEqual(apiForecastResponse1BeachFixture);
   });
+
   it('should return 500 if something goes wrong during the processing', async () => {
     nock('https://api.stormglass.io:443', {
       encodedQueryParams: true,
@@ -49,7 +63,9 @@ describe('Beach forecast functional tests', () => {
       .query({ lat: '-33.792726', lng: '151.289824' })
       .replyWithError('Something went wrong');
 
-    const { status } = await global.testRequest.get(`/forecast`);
+    const { status } = await global.testRequest
+      .get(`/forecast`)
+      .set({ 'x-access-token': token });
 
     expect(status).toBe(500);
   });
