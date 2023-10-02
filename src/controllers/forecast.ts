c629a63b62997d3d2a8abe195739f453cb2416bd
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-10-02 17:07:00 -0300

    ClassMiddleware (authMiddlewar) decoded

--- src/controllers/forecast.ts
+++ src/controllers/forecast.ts
@@ -1,4 +1,5 @@
-import { Controller, Get } from '@overnightjs/core';
+import { ClassMiddleware, Controller, Get } from '@overnightjs/core';
+import { authMiddleware } from '@src/middlewares/auth';
 import { Beach } from '@src/models/beache';
 import { Forecast } from '@src/services/forecast';
 import { Request, Response } from 'express';
@@ -6,14 +7,15 @@ import { Request, Response } from 'express';
 const forecast = new Forecast();
 
 @Controller('forecast')
+@ClassMiddleware(authMiddleware)
 export class ForecastController {
   @Get('')
   public async getForecastForLoggedUser(
-    _: Request,
+    req: Request,
     res: Response
   ): Promise<void> {
     try {
-      const beaches = await Beach.find({});
+      const beaches = await Beach.find({ user: req.decoded?.id });
       const forecastData = await forecast.processForecastForBeaches(beaches);
       res.status(200).send(forecastData);
       // 'unknown' para evitar o erro de tipo desconhecido
