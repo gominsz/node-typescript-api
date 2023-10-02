0cd977247fe6cc17a56ce09341e4688497f71fd8
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-10-02 17:11:22 -0300

    adding user(string) to (Beaches) and fixing (BeachModel) mongoose bug

--- src/models/beache.ts
+++ src/models/beache.ts
@@ -1,4 +1,4 @@
-import mongoose, { Document, Model } from 'mongoose';
+import mongoose, { Document, Model, Schema } from 'mongoose';
 
 export enum BeachPosition {
   S = 'S',
@@ -13,14 +13,16 @@ export interface Beach {
   position: BeachPosition;
   lat: number;
   lng: number;
+  user: string;
 }
 
-const schema = new mongoose.Schema<Beach>(
+const schema = new mongoose.Schema(
   {
     lat: { type: Number, required: true },
     lng: { type: Number, required: true },
     name: { type: String, required: true },
     position: { type: String, required: true },
+    user: { type: Schema.Types.ObjectId, ref: 'User', required: true },
   },
   {
     toJSON: {
@@ -33,5 +35,9 @@ const schema = new mongoose.Schema<Beach>(
   }
 );
 
-//interface BeachModel extends Omit<Beach, '_id'>, Document {}
-export const Beach: Model<Beach> = mongoose.model('Beach', schema);
+interface BeachModel extends Omit<Beach, '_id'>, Document {}
+
+export const Beach: Model<BeachModel> = mongoose.model<BeachModel>(
+  'Beach',
+  schema
+);
