65aa2660d3779b863805550bf0041d662b180262
Matheus Camargo <matheuscamargodealencar@gmail.com>
2023-10-02 17:06:03 -0300

    add ClassMiddleware (authMiddleware) and res status (Internal Server Error)  ValidationError beaches

--- src/controllers/beaches.ts
+++ src/controllers/beaches.ts
@@ -1,20 +1,21 @@
-import { Controller, Post } from '@overnightjs/core';
-import { Beach } from '@src/models/beache';
+import { Controller, Post, ClassMiddleware } from '@overnightjs/core';
 import { Request, Response } from 'express';
+import { Beach } from '@src/models/beache';
 import mongoose from 'mongoose';
+import { authMiddleware } from '@src/middlewares/auth';
 
 @Controller('beaches')
+@ClassMiddleware(authMiddleware)
 export class BeachesController {
   @Post('')
   public async create(req: Request, res: Response): Promise<void> {
     try {
-      const beach = new Beach(req.body);
+      const beach = new Beach({ ...req.body, ...{ user: req.decoded?.id } });
       const result = await beach.save();
       res.status(201).send(result);
-    } catch (error: unknown) {
-      // 'unknown' para evitar o erro de tipo desconhecido
+    } catch (error) {
       if (error instanceof mongoose.Error.ValidationError) {
-        res.status(422).send({ error: (error as Error).message });
+        res.status(422).send({ error: error.message });
       } else {
         res.status(500).send({ error: 'Internal Server Error' });
       }
